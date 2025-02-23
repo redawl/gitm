fd07edfac915e32bada94b914021744de31007b0
Eli Burch <eli.burch@burchbytes.com>
2025-02-23 11:29:26 -0800

    Add placeholder gopher icon

--- ui/packetdisplay.go
+++ ui/packetdisplay.go
@@ -10,9 +10,15 @@ type PacketDisplay struct {
 }
 
 func NewPacketDisplay() *PacketDisplay {
-    return &PacketDisplay{
+    packetDisplay := &PacketDisplay{
         Entry: *widget.NewMultiLineEntry(),
     }
+    packetDisplay.Wrapping = fyne.TextWrapBreak
+    packetDisplay.TextStyle = fyne.TextStyle{
+        Monospace: true,
+    }
+
+    return packetDisplay
 }
 
 func (p *PacketDisplay) TypedRune (r rune) {}
